8f345de3c22c0e81a8cba99ed37ecff19f72dfef
Zachary Coleman <zacharywcoleman@gmail.com>
2021-09-04 15:47:24 -0500

    small update

--- umm-cli-helper/command.py
+++ umm-cli-helper/command.py
@@ -3,10 +3,13 @@ from typing import List
 
 
 class Command:
-    def __init__(self, command: List[str], tags: List[str], id: str = None):
+    def __init__(
+        self, command: List[str], tags: List[str], id: str = None, freq: int = None
+    ):
         """"""
         assert len(command) > 0, "command length 0"
         assert len(tags) > 0, "tags length 0"
         self.command = command
         self.tags = tags
         self.id = id if id else uuid.uuid4()
+        self.freq = freq if freq else 0
