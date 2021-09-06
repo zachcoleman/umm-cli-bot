ddadf7ab9529d1471a60a8f830b769d055c47e38
Zachary Coleman <zacharywcoleman@gmail.com>
2021-09-05 22:47:42 -0500

    small error

--- umm/server/utils.py
+++ umm/server/utils.py
@@ -10,8 +10,8 @@ def parse_commands(path: str):
     with open(path) as f:
         comm_dict = yaml.full_load(f)
     commands = []
-    for k, v in comm_dict["commands"].items():
-        commands.append(Command(id=k, **v))
+    for _, v in comm_dict["commands"].items():
+        commands.append(Command(**v))
 
     return CommandSet(commands)
 
